35c7c3570e330e6598a5662a82e6800f44d3077a
José Sánchez-Gallego <gallegoj@uw.edu>
2021-07-20 11:24:02 -0700

    Add placeholder test

---
+++ thorcam/test_thorcam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2021-07-20
+# @Filename: test_thorcam.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+def test_thorcam():
+    assert True
