f19a85851bec184fb38f148aa5ba1df378688914
José Sánchez-Gallego <gallegoj@uw.edu>
2021-07-21 18:57:43 -0700

    Replace Thorlabs SDK with custom ctypes

---
+++ thorcam/exceptions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2021-07-21
+# @Filename: exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+class SDKError(Exception):
+    """An exception was raised in the Thorlabs SDK."""
+
+    pass
